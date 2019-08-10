560374e51ac4ef59b49a469b1cd00284888a44ee
shawn <shawnbluce@gmail.com>
2019-08-10 16:58:52 +0800

    feat: 修改ALLOW_HOST

--- gg_server/settings.py
+++ gg_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!=9ba--dc$i4s(jdnoueuz%qb+0tmd^0rkw+u@84vu=*-vzeyc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
